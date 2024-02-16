8796157972cae7c639f8c37234fba156e341824a
GitGuru42 <160261252+GitGuru42@users.noreply.github.com>
2024-02-16 23:32:27 +0600

    Add files via upload

---
+++ script 2 страница 2.js
@@ -0,0 +1,16 @@
+var flag = 1;
+ 
+function t()
+    {
+    if (flag == 1) N.style.left = "250px";
+    if (flag == 2) N.style.left = "100px";
+    if (flag == 3) N.style.left = "200px";
+    flag = flag + 1;
+    if (flag == 4) flag = 1;
+    }
+ 
+function al()
+    {
+    alert("Hooray, I knew you'd say yes, I didn't even doubt it))")
+    window.location.href='страница 3 подарок.html'
+    }
---
+++ script welcome proba.js
@@ -0,0 +1,4 @@
+function goToPage() {
+    // Замените 'destination.html' на путь к целевой странице
+    window.location.href = 'подарок 2.html';
+}
